3ed05f675b2089a23c0c5b31e62dc75b6b89ecb2
YuChenLi <swulll123@163.com>
2018-01-22 12:27:15 +0800

    update 1.0.0

--- src/index.vue
+++ src/index.vue
@@ -302,7 +302,7 @@
   width:5px;
   right: 0;
   top:0;
-  bottom: 0px;
+  bottom: 0;
 }
 .bottom-track {
   position: absolute;
@@ -318,6 +318,7 @@
   display: block;
   border-radius: 2px;
   outline: none;
+  margin: 0;
 }
 .left-bar{
   width:100%;
